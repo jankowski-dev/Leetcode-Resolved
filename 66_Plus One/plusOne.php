bc6d7726766a5b2e30d511c8086c854fa21230f3
jankowski-dev <yankowski.dev@gmail.com>
2023-05-30 18:12:20 +0000

    66 Plus One

--- 66_Plus One/plusOne.php
+++ 66_Plus One/plusOne.php
@@ -1,11 +1,4 @@
 <?php
-header('Content-Type:text/html;charset=utf-8');
-// Controll Error
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ERROR);
-
-$digits = [7, 2, 8, 5, 0, 9, 1, 2, 9, 5, 3, 6, 6, 7, 3, 2, 8, 4, 3, 7, 9, 5, 7, 7, 4, 7, 4, 9, 4, 7, 0, 1, 1, 1, 7, 4, 0, 0, 6];
 
 function plusOne($digits)
 {
@@ -16,12 +9,10 @@ function plusOne($digits)
     foreach ($digits as $number) {
         $str .= strval($number);
     }
-    echo $str . "<br>";
+
     $precision = 0;
     $numeric = bcadd($str, 1, 0);
-    echo $numeric;
 
-    // $numeric = intval($str) + 1;
     $str = strval($numeric);
 
     for ($i = 0; $i <= strlen($str); $i++) {
@@ -32,5 +23,3 @@ function plusOne($digits)
 
     return $arrayNumber;
 }
-
-print_r(plusOne($digits));
