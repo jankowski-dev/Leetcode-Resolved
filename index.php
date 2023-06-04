90407743f06b0c4c3bde67adb42e4b9ee021d318
jankowski-dev <yankowski.dev@gmail.com>
2023-06-04 18:41:08 +0000

    414_Third Maximum Number

--- index.php
+++ index.php
@@ -7,18 +7,3 @@ error_reporting(E_ERROR);
 
 $nums = [3,2,1, 4, 5];
 $number = 0;
-
-  function thirdMax($nums) {
-    $unique_nums = array_unique($nums);
-    rsort($unique_nums);
-    $count = count($unique_nums);
-    if ($count >= 3) {
-        return $unique_nums[2];
-    } elseif ($count == 2) {
-        return $unique_nums[0];
-    } elseif ($count == 1) {
-        return $unique_nums[0];
-    }
-    };
-
-print_r(thirdMax($nums));
